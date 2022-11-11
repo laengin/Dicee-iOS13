0737d7525da4487cfc0cdfdfc1a2b3563eb21b3b
laengin <hd.laengin@gmx.de>
2022-11-11 10:28:11 +0100

    Finished Dice App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,25 +12,21 @@ class ViewController: UIViewController {
     // IBOutslet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        // diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-        // diceImageView2.alpha = 0.5
-    }
-    
+
+    /*
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // who         what    value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        // diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        // diceImageView2.alpha = 0.5
     }
-
+    */
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+        let diceArray =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement() //same as diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray.randomElement() //diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+    }
 
 }
 
